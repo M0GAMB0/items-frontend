d89d289851af2ca6bfa4454df5b9773473a9194a
Saikumar Hangal <saikumarhangal99@gmail.com>
2023-05-11 16:37:28 +0530

    create responsive updated

--- src/Create.js
+++ src/Create.js
@@ -14,16 +14,12 @@ const Create = () => {
           className="w-0 border p-2 p-md-5 rounded-4 shadow-lg"
           style={{ background: "#003459" }}
         >
-          <Form
-            className="d-flex flex-column align-items-center justify-content-center p-1 w-100"
-            style={{ border: "2px solid green" }}
-          >
+          <Form className="d-flex flex-column align-items-center justify-content-center p-1 w-100">
             <Form.Group
               className="mb-3 d-flex w-100 justify-content-between align-items-start mx-5 flex-column flex-md-row"
               controlId="exampleForm.ControlInput1"
-              style={{ border: "2px solid red" }}
             >
-              <Form.Label className="w-50 text-te fs-6 fs-md-2 fw-bolder">
+              <Form.Label className="w-50 text-white fs-5 fw-bolder">
                 Item name :&nbsp;
               </Form.Label>
               <Form.Control type="text" placeholder="Enter Item Name" />
@@ -32,10 +28,9 @@ const Create = () => {
               className="mb-3 d-flex w-100 align-items-start justify-content-between mx-5 flex-column flex-md-row align-items-md-center justify-content-between"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label className="w-50 text-white fs-6 fw-bolder">
+              <Form.Label className="w-50 text-white fs-5 fw-bolder">
                 Description :&nbsp;&nbsp;
               </Form.Label>
-              whi
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
             <Form.Group
