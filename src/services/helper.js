fd0e9083b4b2225e485427723f16c0ba035c53b8
Saikumar Hangal⚡ <saikumarhangal@gmail.com>
2023-05-14 11:19:10 +0530

    Update helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const BASE_URL = "items-backend-production.up.railway.app";
+//It is base dns we will use to fetch data from api
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
 });
+//using axios we will fetch from backend
