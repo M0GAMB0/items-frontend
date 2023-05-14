b5b6de1c82cf5b926e9d5e73ea91d6a883ebea70
Saikumar Hangal⚡ <saikumarhangal@gmail.com>
2023-05-14 11:23:02 +0530

    Update Create.js

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -4,6 +4,7 @@ import AddForm from "../Components/AddForm";
 const Create = () => {
   return (
     <Container>
+    //this is component we use to add items in list
       <AddForm />
     </Container>
   );
