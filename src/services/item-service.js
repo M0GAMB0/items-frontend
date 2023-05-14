975c9d2077208aeb3b85824195c438979671d9a1
Saikumar Hangal⚡ <saikumarhangal@gmail.com>
2023-05-14 11:20:24 +0530

    Update item-service.js

--- src/services/item-service.js
+++ src/services/item-service.js
@@ -1,13 +1,16 @@
 import { myAxios } from "./helper";
 
+//api for add items
 export const add_Item = async (item) => {
   const response = await myAxios.post("/api/v1/item/add", item);
   return response.data;
 };
+//api for getting all items from list
 export const getItems = async () => {
   const response = await myAxios.get("/api/v1/item/viewAll");
   return response.data;
 };
+//api for search items by both name and description
 export const searchItems = async (keyword) => {
   const response = await myAxios.get("/api/v1/item/search-item", {
     params: {
